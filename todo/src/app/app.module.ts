8d20cfdcb3753d8f90f56bf287022489353ca552
cristophol <thomas.cristophol@gmail.com>
2017-09-21 15:38:27 +0200

    commit work

---
+++ todo/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+
+export const firebaseConfig = {
+  
+    apiKey: "AIzaSyCtqEdy1GwzCxCWtgeSdVJed8_L3ueLOjw",
+    authDomain: "todo-angular-b3c7b.firebaseapp.com",
+    databaseURL: "https://todo-angular-b3c7b.firebaseio.com",
+    projectId: "todo-angular-b3c7b",
+    storageBucket: "",
+    messagingSenderId: "1091973964176"
+
+};
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+      FormsModule,
+      HttpModule,
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFireDatabaseModule,
+      AngularFireAuthModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
